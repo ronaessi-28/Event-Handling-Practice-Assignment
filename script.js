9994bc8d0ae03c1c676111ab717a6e8a14c789fd
Viney Chhillar <119031478+ronaessi-28@users.noreply.github.com>
2024-11-25 01:46:59 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,39 @@
+// Function to change button color on click
+function changeColor(event) {
+    event.target.style.backgroundColor = 'lightblue';
+}
+
+// Function to show message near the button on mouseover
+function showMessage(event) {
+    var buttonId = event.target.id;
+    var message = document.getElementById('message' + buttonId.slice(-1));
+    message.style.display = 'block';
+}
+
+// Function to hide message on mouseout
+function hideMessage(event) {
+    var buttonId = event.target.id;
+    var message = document.getElementById('message' + buttonId.slice(-1));
+    message.style.display = 'none';
+}
+
+// Function to hide button on double click
+function hideButton(event) {
+    event.target.style.display = 'none';
+}
+
+// Event listeners for each button
+document.getElementById('button1').addEventListener('click', changeColor);
+document.getElementById('button1').addEventListener('mouseover', showMessage);
+document.getElementById('button1').addEventListener('mouseout', hideMessage);
+document.getElementById('button1').addEventListener('dblclick', hideButton);
+
+document.getElementById('button2').addEventListener('click', changeColor);
+document.getElementById('button2').addEventListener('mouseover', showMessage);
+document.getElementById('button2').addEventListener('mouseout', hideMessage);
+document.getElementById('button2').addEventListener('dblclick', hideButton);
+
+document.getElementById('button3').addEventListener('click', changeColor);
+document.getElementById('button3').addEventListener('mouseover', showMessage);
+document.getElementById('button3').addEventListener('mouseout', hideMessage);
+document.getElementById('button3').addEventListener('dblclick', hideButton);
